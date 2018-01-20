fef2e358556322a698264eaabf6d2da9978f58b8
Brecht Savelkoul <brecht.savelkoul@freebility.be>
2018-01-20 11:22:41 +0100

    Move `from-file` logic to index.js

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-module.exports.fromFile = require('./lib/from-file')
-module.exports.toFeed = require('/lib/to-feed')
-module.exports.toWeb = require('./lib/to-web')
+var assert = require('assert')
+var marked = require('marked')
+var path = require('path')
+var pbox = require('pbox')
+var through = require('through2')
+var vinyl = require('./lib/vinyl')
+
+class VinylPress {
+  constructor (config) {
+    assert.equal(typeof config.baseUrl, 'string', 'Site needs a baseUrl')
+  }
+
+  fromFile (opts) {
+    if (!opts) {
+      opts = {}
+    }
+    var collected = []
+    var stringify = (data) => JSON.stringify(data, opts.replacer, opts.space)
+
+    return through.obj(function (doc, _enc, cb) {
+      var parsed = pbox.parse(doc.contents.toString(), {
+        content: content => content.map(section => marked(section)).join('<hr>'),
+        date: date => new Date(date),
+        permalink: permalink => permalink ? permalink : path.basename(doc.path, '.md')
+      })
+
+      if (!opts.concat && !opts.sort) {
+        return cb(null, vinyl(doc.path, stringify(parsed)))
+      }
+      collected = collected.concat(parsed)
+      cb()
+    }, function (cb) {
+      if (!collected.length) {
+        return cb()
+      }
+      if (opts.sort) collected.sort(opts.sort)
+      if (opts.reverse) collected.reverse()
+
+      cb(null, vinyl(opts.concat, stringify(collected)))
+    })
+  }
+}
