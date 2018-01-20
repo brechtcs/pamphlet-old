6a8333c4bffa492a00fd4fbcf948533070f2e867
Brecht Savelkoul <brecht.savelkoul@freebility.be>
2018-01-20 13:18:17 +0100

    Move `to-html` logic to index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var assert = require('assert')
 var atom = require('./lib/atom')
+var fs = require('fs')
+var html = require('./lib/html')
 var marked = require('marked')
 var path = require('path')
 var pbox = require('pbox')
 var through = require('through2')
 var vinyl = require('./lib/vinyl')
 
-class VinylPress {
+module.exports = class VinylPress {
   constructor (config) {
-    assert.equal(typeof config.baseUrl, 'string', 'Site needs a baseUrl')
+    assert.equal(typeof config.baseUrl, 'string', 'Site needs a base URL')
+    assert.equal(typeof config.feedUrl, 'string', 'Site needs a feed URL')
+    assert.ok(fs.existsSync(config.defaultLayout), 'Site needs a default layout')
+    assert.ok(Array.isArray(config.stylesheets), 'Site needs a list of stylesheets')
+    config.stylesheets.forEach(function (styleUrl) {
+      assert.equal(typeof config.styleUrl, 'string', 'Style URL needs to be a string')
+    })
+    assert.ok(Array.isArray(config.scripts), 'Site needs a list of scripts')
+    config.scripts.forEach(function (scriptUrl) {
+      assert.equal(typeof config.scriptUrl, 'string', 'Script URL needs to be a string')
+    })
+
+    this.config = config
   }
 
   fromFile (opts) {
@@ -62,6 +76,28 @@ class VinylPress {
       cb(null, vinyl(filename, atom.feed(content, updated)))
     })
   }
-}
 
-module.exports = VinylPress
+  toHtml (filename, data) {
+    var press = this
+
+    return through.obj(function (doc, _enc, cb) {
+      var content, file
+      var posts = JSON.parse(doc.contents.toString())
+
+      if (posts.length > 1) {
+        content = posts.map(press.layout).join('\n')
+      } else {
+        data = Object.assign(data, posts[0])
+        content = press.layout(data)
+      }
+      file = filename || path.join(data.permalink || path.basename(doc.path, path.extname(doc.path)), 'index.html')
+      cb(null, vinyl(file, html.page(press.config, data, content)))
+    })
+  }
+
+  layout (post) {
+    var layout = path.join(process.cwd(), post.layout || this.config.defaultLayout)
+    delete require.cache(layout)
+    return require(layout)(post)
+  }
+}
